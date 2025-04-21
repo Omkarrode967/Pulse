c9dc2cb43733615179e319411fbf277c134fdcbf
Omkar <omkarrode967@gmail.com>
2025-04-22 01:26:18 +0530

    Simplify build script to use Render's Java support

--- pul-backend/render-build.sh
+++ pul-backend/render-build.sh
@@ -2,10 +2,6 @@
 # exit on error
 set -o errexit
 
-# Set JAVA_HOME to Java 17
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-
 # make mvnw executable
 chmod +x mvnw
 
