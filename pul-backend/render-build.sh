1213929d9edd00141891bde00844f48c174b3ac9
Omkar <omkarrode967@gmail.com>
2025-04-22 01:24:55 +0530

    Update build script to use Java 17

--- pul-backend/render-build.sh
+++ pul-backend/render-build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Set JAVA_HOME to Java 17
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
 # make mvnw executable
 chmod +x mvnw
 
